2902500a21426935663e52dd307be5d58ea49c72
arvindkumaar <arvind.tomar@gmail.com>
2021-06-25 14:09:46 +0530

    Header Part -1

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,108 +1,96 @@
 import React, {Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import './Header.css';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Modal from 'react-modal';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
-import SearchIcon from "@material-ui/icons/Search";
-import Input from "@material-ui/core/Input";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { Divider } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    appBar: {
-        color: "#edede",
-        width: "100%",
-    },
-    toolBar: {
-        color: "#edede",
-        width: "100%",
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    loginButton: {
-        position: "relative",
-        float: "right",
-        backgroundColor: "#edede",
-    },
-    title: {
-      flexGrow: 1,
-    },
-    search: {
-        position: "relative",
-        borderRadius: "4px",
-        backgroundColor: "#c0c0c0",
-        marginLeft: 0,
-        width: "300px",
-        float: "right",
-        marginTop: "18px",
-    },
-    searchIcon: {
-        padding: theme.spacing(2),
-        marginRight: theme.spacing(2),
-        height: "100%",
-        position: "relative",
-        pointerEvents: "none",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        
-    },
-    inputRoot: {
-        color: "#ffffff",
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    },
-  }));
+const customStyles= {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    }
+}
 
-  class Header extends Component {
+const TabContainer = function(props) {
+    return(
+        <Typography component="div" style={{padding: 0}}>
+            {props.children}
+        </Typography>
+    );
+}
+
+class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            modalIsOpen: false,
+            value: 0,
+        };
+    }
+
+    openModalHandler = () => {
+        this.setState({modalIsOpen: true})
+    }
+
+    closeModalHandler = () => {
+        this.setState({modalIsOpen: false})
+    }
+
+    tabChangeHandler = (event, value) => {
+        this.setState({value});
+    }
+
     render() {
-        const { classes } = this.props;
-        
+        // const { classes } = this.props;
         return (
-          <AppBar position="static" className={classes.appBar} style={{ background: '#212b35' }} >
-            <Toolbar style={{ width: '100%' }}>
-              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                <FastfoodIcon />
-              </IconButton>
-              <div className={classes.search} style={{ background: '#ccfff5' }} >
-                                
-                                <span >
-                                <SearchIcon className={classes.searchIcon} />
-                                </span>
-                                
-                                
-                                <Input
-                                    disableUnderline={false}
-                                    placeholder="Search by Restaurant Name"
-                                    classes={{
-                                        root: classes.inputRoot,
-                                        input: classes.inputInput,
-                                    }}
-                                    inputProps={{ "aria-label": "search" }}
-                                    onChange={this.inputChangeHandler}
-                                    style={{ color: '#c4c4c4' }}
-                                />
-                </div>
-              <Button className={classes.loginButton} style={{ background: '#c4c4c4' }} >
-              <AccountCircle />
-                  Login
-                  </Button>
-            </Toolbar>
-          </AppBar>
-        );
-      }
-  }
-export default withStyles(useStyles) (Header);
+            <div>
+                <header className="app-header">
+                    <IconButton  style={{color: "#ffffff"}} aria-label="Menu">
+                    <FastfoodIcon style={{color: "#ffffff"}} />
+                    </IconButton>
+                    <div className="login-button">
+                        <Button variant="contained" color="default" startIcon={<AccountCircle />} onClick={this.openModalHandler}>
+                            Login
+                        </Button>
+                    </div>
+                </header>
+                <Modal 
+                ariaHideApp={false} 
+                isOpen={this.state.modalIsOpen} 
+                contentLabel="Login"
+                onRequestClose={this.closeModalHandler}
+                style={customStyles}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="LOGIN" />
+                        <Tab label="SIGNUP"/>
+                    </Tabs>
+                    <TabContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="userName">Username</InputLabel>
+                            <Input id="userName" type="text"/>
+                        </FormControl>
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password"/>
+                        </FormControl>
+                    </TabContainer>
+
+                </Modal>
+            </div>
+        )
+    }
+}
+export default Header;
