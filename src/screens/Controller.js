2e869b02f76a1fb39ea8e7d363db573c5e421946
arvindkumaar <arvind.tomar@gmail.com>
2021-06-28 16:35:50 +0530

    Minor Corrections

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Checkout from "../screens/checkout/Checkout";
 import Home from "../screens/home/Home";
 import Details from "../screens/details/Details";
-import Profile from "../screens/profile/Profile";
+
 
 // This is a controller component that provides access and routing for the SPA Food Ordering App and its 4 views viz.
 // 1. Home Page ("/")
 // 2. Details Page ("/restaurant/:restaurantId")
 // 3. Checkout Page ("/checkout")
-// 4. Profile Page ("/profile")
+
 class Controller extends Component {
   constructor() {
     super();
@@ -34,11 +34,6 @@ class Controller extends Component {
             exact
             path="/restaurant/:restaurantId"
             render={(props) => <Details {...props} baseUrl={this.baseUrl} />}
-          />
-          <Route
-            exact
-            path="/profile"
-            render={(props) => <Profile {...props} baseUrl={this.baseUrl} />}
           />
         </div>
       </Router>
